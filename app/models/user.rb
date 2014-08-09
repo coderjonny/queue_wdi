301f2a7b911b58c56036978dd16e33c03c8f8229
jonny kang <jono.kang@gmail.com>
2014-08-08 18:51:03 -0700

    only admins can delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User
   field :email, type: String
   field :github_handle, type: String
   field :password_digest, type: String
+  field :is_admin, type: Boolean
 
   validates :name, presence: true
+
   validates :email, presence: true
+  validates_uniqueness_of :email
+
   validates :github_handle, presence: true
 
   has_many :turns
